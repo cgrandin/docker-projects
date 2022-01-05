019be870915f1465b81b0bd67481faaf9f170b3d
Chris Grandin <chris.grandin@gmail.com>
2022-01-04 19:40:18 -0800

    Compile SS in optimized mode * Use output_to_seperate_files branch

--- ss/install_ss.sh
+++ ss/install_ss.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Clone SS source and compile with optimization for ss docker image
+
 install_dir=/usr/bin
 ss_dir=$install_dir/ss
 
 cd $install_dir
-git clone --branch main https://github.com/nmfs-stock-synthesis/stock-synthesis ss
+git clone --branch output_to_separate_files https://github.com/nmfs-stock-synthesis/stock-synthesis ss
 cd $ss_dir
 # Checking out 2021's base model commit
 #git checkout c6c9178 # Commit where tag was changed to 3.30.16.03 (Oct 19, 2020)
-git checkout f65ff56 # Dec 15, 2020 (36 commits after change to 3.30.16.03)
+#git checkout f65ff56 # Dec 15, 2020 (36 commits after change to 3.30.16.03) use this for 2021 assessment
 #git checkout 33108e9 # Dec 18, 2020
 #git checkout 02b927a # Dec 23, 2020
 cat \
@@ -35,5 +37,5 @@ SS_ALK.tpl \
 SS_timevaryparm.tpl \
 SS_tagrecap.tpl > ss.tpl
 
-admb ss
+admb -f ss
 
